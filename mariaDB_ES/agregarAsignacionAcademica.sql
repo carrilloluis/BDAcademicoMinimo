d9a26a7466fa754a94756a8b32603fcd702d0bab
Luis Carrillo Gutiérrez <carrilloluis@users.noreply.github.com>
2022-02-26 19:26:49 -0500

    Update agregarAsignacionAcademica.sql

--- mariaDB_ES/agregarAsignacionAcademica.sql
+++ mariaDB_ES/agregarAsignacionAcademica.sql
@@ -3,9 +3,9 @@
 -- Creado	2018/01/09
 -- ---------------------------------------------------------------------
 
-DROP PROCEDURE IF EXISTS `spAddIntoEF`;
+DROP PROCEDURE IF EXISTS `sp0005`;
 DELIMITER $$
-CREATE PROCEDURE IF NOT EXISTS `spAddIntoEF` ( -- agregarAsignaciónAcadémica
+CREATE PROCEDURE IF NOT EXISTS `sp0005` ( -- agregarAsignaciónAcadémica
 	IN `@a` CHAR(5) CHARACTER SET utf8,
 	IN `@b` CHAR(6) CHARACTER SET utf8
 ) BEGIN
